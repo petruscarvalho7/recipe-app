55d6a068a0fdb43cd32b49b25c3095f97ce0aff2
petrus.carvalho <petrus.carvalho@logirix.com>
2023-08-05 14:01:13 +0100

    Initial Commit

---
+++ AnimationRecipeApp/AnimationRecipeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationRecipeAppApp.swift
+//  AnimationRecipeApp
+//
+//  Created by Petrus Carvalho on 05/08/23.
+//
+
+import SwiftUI
+
+@main
+struct AnimationRecipeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimationRecipeAppUITests/AnimationRecipeAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  AnimationRecipeAppUITestsLaunchTests.swift
+//  AnimationRecipeAppUITests
+//
+//  Created by Petrus Carvalho on 05/08/23.
+//
+
+import XCTest
+
+final class AnimationRecipeAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
